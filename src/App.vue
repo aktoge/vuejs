b7c9d2352759cd238f9324e8905d621002018606
aktoge <ak.toge@gmail.com>
2022-01-23 16:45:20 +0900

    file read

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    <form>
+    <!-- <form>
       <input type="file" @change="readfile" />
-    </form>
-    <div>{{ data }}</div>
+    </form> -->
+    <div id="upload" class="commonStyle" :class="{'stylePre':stylePre, 'styleDrag':styleDrag , 'stylePost':stylePost}"
+        @dragover.prevent="changeStyle($event,'ok')"
+        @dragleave.prevent="changeStyle($event,'no')"
+        @drop.prevent="uploadFile($event)">
+
+        <form for="inputFileByDrop" class="button"></form>
+        <input id="inputFileByDrop" type="file" @change="uploadFile($event)">
+        <p>またはここにファイルをドラッグ＆ドロップ</p>
+
+    </div>
+
+    <div>DATA:{{ textData }}</div>
+    <div>Prop:{{ fileName }}</div>
+
+    <button class="btn btn-success">アップロード</button>
+
   </div>
 </template>
 
@@ -11,20 +26,64 @@
 export default {
   data(){
     return {
-      data: []
+      textData: [] ,
+      fileName: 'none',
+      stylePre: true,
+      styleDrag: false,
+      stylePost: false
     }
   },
   methods: {
-    readfile: function(e) {
-      const vm = this
-      const file = e.target.files[0]
-      const reader = new FileReader()
+    // readfile: function(e) {
+    //   const vm = this
+    //   const file = e.target.files[0]
+    //   const reader = new FileReader()
 
+    //   reader.onload = function() {
+    //     vm.textData = reader.result
+    //   }
+    //   reader.readAsText( file )
+    // },
+    uploadFile: function(event){
+      // console.log(event)
+      this.stylePre = false
+      this.styleDrag = false
+      this.stylePost = true
+      const vm = this
+      const fileList = event.target.files ? event.target.files : event.dataTransfer.files
+      const file = fileList[0]
+      const reader = new FileReader()
       reader.onload = function() {
-        vm.data = reader.result
+        vm.textData = reader.result
       }
       reader.readAsText( file )
-    }
+      this.fileName = file.name
+    },
+    changeStyle: function(event,flag){
+      if(flag=='ok'){
+        this.styleDrag = true
+      }else{
+        this.styleDrag = false
+      }
+    },
   }
 }
 </script>
+
+<style scoped>
+.commonStyle { text-align:center; width:400px;}
+.stylePre {border:3px dotted red;}
+.stylePost {border:3px dotted blue;}
+.styleDrag {border:3px dotted green;}
+
+.btn.btn-success {
+        width:380px;
+        margin:10px;
+        background-position:center;
+}
+
+#inputFileByDrop {
+    display:block;
+    margin-left:40px;
+}
+</style>
